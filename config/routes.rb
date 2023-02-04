327383da5a5584f97078b83eba783a4704d4dc98
Pradeep Rao Rayapudi <pradeep.rayapudi93@gmail.com>
2023-02-04 20:07:32 +0000

    devise add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, defaults: { format: :json }        
   resources :people
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
